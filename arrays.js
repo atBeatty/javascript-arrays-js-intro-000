43bdfe690c64ed59361275c6e6337500f136ebf0
Adam Beatty <adam.t.beatt@gmail.com>
2020-07-15 04:42:05 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,12 @@
 var chocolateBars;
- chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element) {
+  var newArray = [element, ...array];
+  return newArray;
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+    return array.unshift(element);
+}
