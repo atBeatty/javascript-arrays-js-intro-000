945473dd76b998297e65788de00cc7207fa40675
Adam Beatty <adam.t.beatt@gmail.com>
2020-07-15 04:44:16 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,3 +10,12 @@ function addElementToBeginningOfArray(array, element) {
 function destructivelyAddElementToBeginningOfArray(array, element) {
     return array.unshift(element);
 }
+
+function addElementToEndOfArray(array, element) {
+  var newArray = [...array, element];
+  return newArray;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return array.pop();
+}
