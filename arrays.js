5a041824bb81d111f4b3fc59b3feff0c66919e39
Adam Beatty <adam.t.beatt@gmail.com>
2020-07-15 04:50:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -25,8 +25,9 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.shift();
-}
+  array.shift();
+  return array;
+  }
 
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1);
